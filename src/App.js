d971d43c7a7f5b67a38e1099b72274be08851da5
Son Roy Almerol <sonroya.almerol@gmail.com>
2021-08-05 13:58:48 +0800

    add suspense

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Navbar from './Components/Navbar';
+import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 const Clusters = React.lazy(() => import('./Pages/Clusters'));
 const Home = React.lazy(() => import('./Pages/Home'));
 const TWG = React.lazy(() => import('./Pages/TWG'));
 const Schedule = React.lazy(() => import('./Pages/Schedule'));
 const Tv = React.lazy(() => import('./Pages/Tv'));
+
+const Loading = () => {
+    return (
+        <Grid container justifyContent="center" alignItems="center">
+            <Grid item>
+                <CircularProgress color="primary" />
+            </Grid>
+        </Grid>
+    )
+}
 
 function App() {
     const [tabValue, setTabValue] = React.useState(0);
@@ -14,22 +27,32 @@ function App() {
             <main id='home'>
                 <Navbar tabValue={tabValue} setTabValue={setTabValue} />
                 <section className='home'>
-                    <Home onEnterViewport={() => setTabValue(0)} />
+                    <Suspense fallback={<Loading />}>
+                        <Home onEnterViewport={() => setTabValue(0)} />
+                    </Suspense>
                 </section>
                 <section className='clusters' id='clusters'>
-                    <Clusters onEnterViewport={() => setTabValue(1)} />
+                    <Suspense fallback={<Loading />}>
+                        <Clusters onEnterViewport={() => setTabValue(1)} />
+                    </Suspense>
                 </section>
                 <section className='schedule' id='schedule'>
                     {/* <h1>Schedule</h1> */}
-                    <Schedule onEnterViewport={() => setTabValue(2)} />
+                    <Suspense fallback={<Loading />}>
+                        <Schedule onEnterViewport={() => setTabValue(2)} />
+                    </Suspense>
                 </section>
                 <section className='samahanTv' id='samahanTv'>
                     {/* <h1>SAMAHAN TV</h1> */}
-                    <Tv onEnterViewport={() => setTabValue(3)} />
+                    <Suspense fallback={<Loading />}>
+                        <Tv onEnterViewport={() => setTabValue(3)} />
+                    </Suspense>
                 </section>
                 <section className='fiestaTWG' id='fiestaTWG'>
                     {/* <h1>Fiesta TWG</h1> */}
-                    <TWG onEnterViewport={() => setTabValue(4)} />
+                    <Suspense fallback={<Loading />}>
+                        <TWG onEnterViewport={() => setTabValue(4)} />
+                    </Suspense>
                 </section>
                 <section className='rest'></section>
             </main>
