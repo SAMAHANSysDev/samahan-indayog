0656630ea19c8b5bfd5ea76b69df293bd36d5ce2
janna <rijanuy88@gmail.com>
2021-08-05 18:45:22 +0800

    ADDED: Schedule

---
+++ src/Components/liveData.js
@@ -0,0 +1,20 @@
+export const liveData = [
+    {
+        id:1,
+        link:""
+    },
+    {
+        id:2,
+        link:""
+    },
+
+    {
+        id:3,
+        link:""
+    },
+    {
+        id:4,
+        link:""
+    },
+
+]
--- src/Pages/Schedule.js
+++ src/Pages/Schedule.js
@@ -1,9 +1,10 @@
 import React from 'react'
 // import Typography from '@material-ui/core/Typography';
-import { Grid } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 // import Buttons from '../Components/Buttons';
 import { makeStyles } from "@material-ui/core/styles";
 import handleViewport from 'react-in-viewport';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) => ({
     rootContainer: {
@@ -15,32 +16,94 @@ const useStyles = makeStyles((theme) => ({
         backgroundSize: 'cover',
         backgroundPosition: 'center'
     },
+    container:{
+        minHeight: '75vh',
+        padding: theme.spacing(4),
+        paddingTop: '4rem',
+        paddingBottom: '4rem',
+        backgroundImage: 'url("/assets/Backgrounds/Live Now BG.png")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+    },
+    box: {
+        backgroundColor: "white",
+        borderRadius: 26,
+        height: "100%",
+        padding: theme.spacing(6),
+    },
+    buttonStyle: {
+        borderRadius: 26,
+        backgroundColor: 'white',
+        padding: theme.spacing(2),
+        marginRight:"2%",
+        marginBottom:"3%"
+    }
 }));
 
 function Schedule({ forwardedRef }) {
     const classes = useStyles();
 
     return (
-        <Grid innerRef={forwardedRef} className={classes.rootContainer}>
-            
+        <>
+        <Grid container direction="row" justifyContent="flex-end" innerRef={forwardedRef} className={classes.container} spacing={2}>
+            <Grid direction="column" justifyContent="center">
+                <Grid >
+                    <Button className={classes.buttonStyle} style={{marginTop:"150%"}}>
+                        LIVE NOW
+                    </Button>
+                </Grid>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+                <Paper elevation={2} className={classes.box}>
+                    
+                </Paper>
+            </Grid>
         </Grid>
 
+        <Grid container direction="row" innerRef={forwardedRef} className={classes.rootContainer}>
+            <Grid item>
+                <img 
+                        src='/assets/Avatars/Atenean.png' 
+                        alt="Atenean" 
+                        width="550" 
+                />
+            </Grid>
 
-        // <div className='schedule'>
-        //     <div className="schedule_left">
-        //         {/* character/element  */}
-        //         <img src="https://venngage-wordpress-gallery.s3.amazonaws.com/uploads/2018/10/25.jpg" alt="element/character" />
-        //     </div>
-        //     <div className="schedule_right">
-        //         <div className="schedule_right_top">
-        //             {/* fb link */}
-        //             <Typography variant="h3">live</Typography>
-        //         </div>
-        //         <div className="schedule_right_bottom">
-        //             {/* sched day 123 */}
-        //         </div>
-        //     </div>
-        // </div>
+            <Grid item direction="column">
+                {/* <Grid direction="row" justifyContent="center" > */}
+
+                <Grid spacing={3} container direction="row" justifyContent="flex-start" alignItems="center" > 
+                    <Button className={classes.buttonStyle}>
+                        SCHEDULE
+                    </Button>
+                    <Button className={classes.buttonStyle}>
+                        Day 1
+                    </Button>
+                    <Button className={classes.buttonStyle}>
+                        Day 2
+                    </Button>
+                    <Button className={classes.buttonStyle}>
+                        Day 3
+                    </Button>
+                </Grid>
+                
+                {/* <Paper elevation={2} className={classes.box}> */}
+                <Grid spacing={3} container direction="row" justifyContent="flex-start" alignItems="center">
+                    <img 
+                        src='/assets/Schedules/1.png' 
+                        alt="sched1" 
+                        width="900" 
+                    />
+                </Grid>
+                    
+                {/* </Paper> */}
+                
+            </Grid>
+            
+        </Grid>
+        </>
+        
     )
 }
 
