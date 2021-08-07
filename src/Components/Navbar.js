361f4feae613eb6d314aeac08554c26fe2e474f7
Son Roy Almerol <sonroya.almerol@gmail.com>
2021-08-07 11:40:22 +0800

    remove unused dependencies

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,12 +14,8 @@ import MenuIcon from '@material-ui/icons/Menu';
 
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 
 const useStyles = makeStyles((theme) => ({
     root: {
--- src/Components/Schedules.js
+++ src/Components/Schedules.js
@@ -1,42 +1,7 @@
-import { Grid, makeStyles, Paper, Typography } from '@material-ui/core'
+import { Grid, Typography } from '@material-ui/core'
 import React from 'react'
 
-const useStyles = makeStyles((theme) => ({
-    rootContainer: {
-        minHeight: '120vh',
-        padding: theme.spacing(4),
-        paddingTop: '4rem',
-        paddingBottom: '4rem',
-        backgroundImage: 'url("/assets/Backgrounds/Schedule BG.png")',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-    },
-    container:{
-        minHeight: '75vh',
-        padding: theme.spacing(4),
-        paddingTop: '4rem',
-        paddingBottom: '4rem',
-        backgroundImage: 'url("/assets/Backgrounds/Live Now BG.png")',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-    },
-    box: {
-        backgroundColor: "white",
-        borderRadius: 26,
-        height: "100%",
-        padding: theme.spacing(6),
-    },
-    buttonStyle: {
-        borderRadius: 26,
-        backgroundColor: 'white',
-        padding: theme.spacing(2),
-        marginRight:"2%",
-        marginBottom:"3%"
-    }
-}));
 function Schedules({time,data}) {
-    const classes = useStyles();
-
     return (
         <Grid spacing={8} container direction="row" justifyContent="space-between" alignItems="center" >
         {/* <Paper elevation={2} className={classes.box}> */}
