88aba1f754c5b0be488f8d314c23f8c6a539c5dd
janna <rijanuy88@gmail.com>
2021-08-14 21:44:58 +0800

    added: simple modal

--- src/Pages/Clusters.js
+++ src/Pages/Clusters.js
@@ -1,11 +1,49 @@
-import { Grid } from '@material-ui/core'
+import { Button, Grid } from '@material-ui/core'
 import React from 'react'
 import Cards from '../Components/Clusters/Cards'
 import { clustersData } from '../Components/Clusters/ClustersData'
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from "@material-ui/core/styles";
+import {withStyles, makeStyles } from "@material-ui/core/styles";
 import Hidden from '@material-ui/core/Hidden';
 import handleViewport from 'react-in-viewport';
+import { useState } from 'react';
+import Modal from "react-modal";
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 14,
+    },
+    }))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+    root: {
+        '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+        },
+    },
+}))(TableRow);
+
+function createData(cluster,event,link) {
+    return { cluster,event,link };
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+];
+
 
 const useStyles = makeStyles((theme) => ({
     rootContainer: {
@@ -15,6 +53,30 @@ const useStyles = makeStyles((theme) => ({
         backgroundSize: 'cover',
         backgroundPosition: 'center'
     },
+    mymodal: {
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+
+        border: '1px solid #ccc',
+        background: "#fff",
+        overflow: "auto",
+        borderRadius: "4px",
+        outline: "none",
+        padding: "20px",
+    },
+    myoverlay: {
+        position: "fixed",
+        top: "0",
+        left: "0",
+        right: "0",
+        bottom: "0",
+        backgroundColor: "rgba(0, 0, 0, 0.75)",
+    },
+    table: {
+        minWidth: 700,
+    },
 }));
 
 const moduloToMargin = (modulo) => {
@@ -23,17 +85,27 @@ const moduloToMargin = (modulo) => {
 
 function Clusters({ forwardedRef }) {
     const classes = useStyles();
+    const [isOpen, setIsOpen] = useState(false);
+    
+    function toggleModal() {
+        setIsOpen(!isOpen);
+        
+    }
 
     return (
+        <>
         <Grid innerRef={forwardedRef} container direction="column" justifyContent="center" alignItems="center" spacing={1} className={classes.rootContainer}>
             <Grid item>
                 <Hidden mdDown>
                     <Grid container justifyContent="center" alignItems="center" spacing={3}>
                         {clustersData.map((cluster, i) => {
                             return (
-                                <Grid item key={cluster.id}>
-                                    <Cards img={cluster.img} alt={cluster.alt} title={cluster.title} id={cluster.id} style={{ marginTop: moduloToMargin(i % 3) }} />
-                                </Grid>
+                                <Button onClick={toggleModal}>
+                                    <Grid item key={cluster.id}>
+                                        <Cards img={cluster.img} alt={cluster.alt} title={cluster.title} id={cluster.id} style={{ marginTop: moduloToMargin(i % 3) }}/>
+                                    </Grid>
+                                </Button>
+
                             )
                         })}
                     </Grid>
@@ -42,9 +114,11 @@ function Clusters({ forwardedRef }) {
                     <Grid container direction="column" justifyContent="center" alignItems="center" spacing={1}>
                         {clustersData.map((cluster) => {
                             return (
-                                <Grid item key={cluster.id}>
-                                    <Cards img={cluster.img.replace('.png', 'mobile.png')} alt={cluster.alt} title={cluster.title} id={cluster.id} mobile />
-                                </Grid>
+                                <Button onClick={toggleModal}>
+                                    <Grid item key={cluster.id}>
+                                        <Cards img={cluster.img.replace('.png', 'mobile.png')} alt={cluster.alt} title={cluster.title} id={cluster.id} mobile />
+                                    </Grid>
+                                </Button>
                             )
                         })}
                     </Grid>
@@ -61,6 +135,40 @@ function Clusters({ forwardedRef }) {
                 </Grid>
             </Grid>
         </Grid>
+        
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={toggleModal}
+            contentLabel="My dialog"
+            className={classes.mymodal}
+            overlayClassName={classes.myoverlay}
+            closeTimeoutMS={500}
+        >
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="customized table">
+                    <TableHead>
+                    <TableRow>
+                        <StyledTableCell>Cluster</StyledTableCell>
+                        <StyledTableCell align="right">Event</StyledTableCell>
+                        <StyledTableCell align="right">Link</StyledTableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {rows.map((row) => (
+                        <StyledTableRow key={row.cluster}>
+                        <StyledTableCell component="th" scope="row">
+                            {row.cluster}
+                        </StyledTableCell>
+                        <StyledTableCell align="right">{row.event}</StyledTableCell>
+                        <StyledTableCell align="right">{row.link}</StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Modal>
+
+        </>
     )
 }
 
